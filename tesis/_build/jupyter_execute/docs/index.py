9a825882c98831981d405e176cc13b700b81ecde
Argimiro <a.eworo@alumnos.urjc.es>
2021-12-29 18:41:04 +0100

    bibliografia anadida

---
+++ tesis/_build/jupyter_execute/docs/index.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# (index)=
+# 
+# # <center>INTRODUCCIÓN<center/>
+# 
+# El objetivo de este libro electrónico es reunir una serie de ejemplos ilustrativos que demuestren las distintas posibilidades que ofrece la tecnología
+# jupyter y jupyter books para publicación de contenidos de proyectos de ciencias de datos y/o machine learning.
+# 
+# Dichos ejemplos están basados en la reproducibilidad científica. La reproducibilidad es la característica de un proyecto de poder ser recreado, 
+# siguiendo los mismos pasos detallados y con los mismos datos y/o distintos, llegando siempre a resultados idénticos.
+# 
+# Para eso vamos a ilustrar los siguientes proyectos:
+# 
+# - [End To End Machine Learning](https://argimiro-eworo.github.io/TFG/docs/end2end.html): Analizaremos, con datos proporcionados,
+# varios algoritmos de predicción para los precios de viviendas en la costa de california. Nos vamos a basar en el libro {cite}`geron2019hands`.
+# 
+# - [El Tiempo En Nueva York](https://argimiro-eworo.github.io/TFG/docs/Capitulo_altair.html): Con datos brutos del tiempo en Nueva York y haciendo 
+# uso de la herramienta Altair {cite}`vanderplas2018altair`, crearemos una gráfico interactivo que relaciona los meses del año y las temperaturas.
+# 
+# - [Citi Bike en Nueva York](https://argimiro-eworo.github.io/TFG/docs/capitulo_geopandas.html): Con datos brutos del sistema de intercambio de 
+# bicicletas en NY y haciendo uso de la herramienta Geopandas {cite}`jordahl2014geopandas`, ilustraremos sobre un  mapa interactivo, la localización de cada estación así como información del
+# ńumero de llegadas y salidas de bicis en cada una.
+# 
+# ## Bibliografía
+# 
+# ```{bibliography} ../_bibliography/references.bib
+# ```
